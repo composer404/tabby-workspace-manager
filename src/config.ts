2ece5e6789ecfd9dfb770e99e69653fcff79882c
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2021-11-13 02:05:13 +0100

    Cannot set hotkeys for workspace profiles #4

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,18 @@
 import { ConfigProvider } from 'tabby-core';
+import { HOT_KEYS } from './interfaces/interfaces';
 
 export class WorkspaceManagerProvider extends ConfigProvider {
     defaults = {
         workspaceManager: {
             defaultWorkspaceProfile: 0,
             runOnStartup: false,
+        },
+        hotkeys: {
+            [HOT_KEYS.WORKSPACE_PROFILE_1]: [],
+            [HOT_KEYS.WORKSPACE_PROFILE_2]: [],
+            [HOT_KEYS.WORKSPACE_PROFILE_3]: [],
+            [HOT_KEYS.WORKSPACE_PROFILE_4]: [],
+            [HOT_KEYS.WORKSPACE_PROFILE_5]: [],
         },
     };
 }
